8f968736865f7c285cacf78ad4972b6a06f16b08
Torben Rasmussen <rtorben@gmail.com>
2011-04-08 10:27:13 -0700

    added a few more things

--- hw1/script.sh
+++ hw1/script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [-e work]
-then
-    vlib work
-fi
 vlog adder8.sv
-vsim -novopt -do adder8.do adder8.sv
+#vsim -novopt -do adder8.do adder8
+vsim adder8 -do adder8.do -quiet -c -t 1ps
