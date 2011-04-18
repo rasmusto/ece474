97f94f777ebfa387869b450d0b6b4741f569d496
Torben Rasmussen <rtorben@gmail.com>
2011-04-18 10:30:08 -0700

    almost compiling

--- hw2/alu.do
+++ hw2/alu.do
@@ -2,11 +2,6 @@ add list -nodelta
 configure list -strobestart {9 ns}  -strobeperiod {10 ns}
 configure list -usestrobe 1
 
-add list -notrigger -hex -width 4  -label a          a
-add list -notrigger -hex -width 4  -label b          b
-add list -notrigger -hex -width 10 -label sum_out    sum_out
-add list -notrigger -hex -width 8  -label c_out      c_out
-
 view signals
 add wave -r /*
 
--- hw2/alu.sv
+++ hw2/alu.sv
@@ -8,18 +8,14 @@ module alu(
 );
 endmodule
 
-parameter c_add = 4'h01;
-parameter c_sub = 4'h02;
-parameter c_inc = 4'h03;
-
-c_add      =  0001   in_a + in_b
-c_sub      =  0010   in_a - in_b
-c_inc      =  0011   in_a + 1
-c_dec      =  0100   in_a - 1
-c_or       =  0101   in_a OR in_b
-c_and      =  0110   in_a AND in_b
-c_xor      =  0111   in_a XOR in_b
-c_shr      =  1000   in_a is shifted one place right, zero shifted in
-c_shl      =  1001   in_a is shifted one place left, zero shifted in
-c_onescomp =  1010   in_a gets "ones complemented"
-c_twoscomp =  1011   in_a gets "twos complemented"
+parameter c_add         =   4'h1;  //in_a + in
+parameter c_sub         =   4'h2;  //in_a - in
+parameter c_inc         =   4'h3;  //in_a + 1
+parameter c_dec         =   4'h4;  //in_a - 1
+parameter c_or          =   4'h5;  //in_a OR in_b
+parameter c_and         =   4'h6;  //in_a AND in_b
+parameter c_xor         =   4'h7;  //in_a XOR in_b
+parameter c_shr         =   4'h8;  //in_a is shifted one place right, zero shifted in
+parameter c_shl         =   4'h9;  //in_a is shifted one place left, zero shifted in
+parameter c_onescomp    =   4'hA;  //in_a gets "ones complemented"
+parameter c_twoscomp    =   4'hB;  //in_a gets "twos complemented"
