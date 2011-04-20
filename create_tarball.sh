1483f9fe36b78e5280029d41524a402b99299324
Torben Rasmussen <rtorben@gmail.com>
2011-04-20 10:37:16 -0700

    it works!

--- create_tarball.sh
+++ create_tarball.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 if [ $# -ne 1 ]
 then
-    echo "Usage: create_tarball.sh <directory>"
+    echo "Usage: create_tarball.sh [DIRECTORY]"
+    exit
 fi
 if [ -d $1 ]
 then
     echo "$1 exists and is a directory!"
+    tar cvf $1.tar  --exclude=*.log         \
+                    --exclude=*.txt         \
+                    --exclude=*.svf         \
+                    --exclude=*.wlf         \
+                    --exclude=transcript    \
+                    --exclude=work          \
+                    $1
 else
     echo "$1: no such directory"
+    exit
 fi
-tar cvf $1.tar  --exclude=*.log         \
-                --exclude=*.txt         \
-                --exclude=*.svf         \
-                --exclude=*.wlf         \
-                --exclude=transcript    \
-                --exclude=work          \
-                $1
 
-command.log
-default.svf
-doit.sh
-hw2.txt
-rtl_src
-syn_alu
-transcript
-vsim.wlf
-work
