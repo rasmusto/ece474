8bbb0fbdb4aa9eccfa2612f8f7902917b8fb3280
Torben Rasmussen <rtorben@gmail.com>
2011-05-10 23:37:55 -0700

    added shift reg

---
+++ hw4/rtl_src/shift_reg.sv
@@ -0,0 +1,12 @@
+module shift_reg (
+    input           serial_data,
+    input           data_ena,
+    input           clk_50,
+    input           reset_n,
+    output [7:0]    parallel_data
+);
+
+always_ff @ (posedge clk_50, negedge reset_n)
+    if      (!reset_n)   parallel_data <= 8'h00;
+    else if (data_ena)   parallel_data <= {serial_data,parallel_data[7:1]};
+end
